4f7fd35a7869d18dad811bb93e9f7104e856ab4c
jesse keane <me@jessekeane.me>
2014-08-30 12:45:54 -0700

    some updates

--- personal/prefs.el
+++ personal/prefs.el
@@ -1,15 +1,39 @@
 ;; prefs.el -- just setting some preferences
 
 ;;; Code:
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+
+;; be moar like vim + ctrlp plz
 (define-key evil-normal-state-map (kbd "C-p") 'projectile-find-file)
 
+;; wombat is nice
 (load-theme 'wombat t)
 
-(menu-bar-mode -1)
-(require 'column-marker)
-(column-marker-1 80)
+;; syntax checkin'
+(setq-default js2-show-parse-errors nil)
+(setq-default js2-strict-missing-semi-warning nil)
+(setq-default js2-strict-trailing-comma-warning nil)
 
-(require 'js-comint)
+(add-hook 'js2-mode-hook (lambda ()
+                          (setq-default flycheck-eslint-rulesdir
+                            "/Users/jessekeane/Projects/UA/ua-style/lib/rules")
+                          (setq-default flycheck-eslintrc
+                            "/Users/jessekeane/Projects/UA/ua-style/config.json")
+                          (flycheck-select-checker 'javascript-eslint)
+                          (flycheck-mode)))
+
+
+;; ctrlp emu
+(setq projectile-completion-system 'grizzl)
+
+;; stop it, emacs
+(menu-bar-mode -1)
+
+;; 80 column lines are nice.
+(column-marker-2 80)
+
+;; node repl
 (setq inferior-js-program-command "node")
 
 ;; prefs end here
